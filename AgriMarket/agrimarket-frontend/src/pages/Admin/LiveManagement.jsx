dce26aadb1e0161a91fafde0d916892e639477d8
trungkhanh03082005-gif <trungkhanh03082005@gmail.com>
2026-07-04 18:04:46 +0700

    Giao dien khi bi ban

--- AgriMarket/agrimarket-frontend/src/pages/Admin/LiveManagement.jsx
+++ AgriMarket/agrimarket-frontend/src/pages/Admin/LiveManagement.jsx
@@ -241,6 +241,7 @@ export default function LiveManagement() {
           return {
             ...session,
             status: "replay",
+            isBanned: true,
             scheduledTime: "Kết thúc bởi quản trị viên"
           };
         }
